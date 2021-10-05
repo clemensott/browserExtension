9d070090a427a5b013c44b14fa28885301b8ded5
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2021-10-05 17:04:20 +0200

    fixed twitter banner remover

--- customizeWebsitesExtension/twitter_content.js
+++ customizeWebsitesExtension/twitter_content.js
@@ -1,6 +1,14 @@
+function equalsLink(href, path) {
+    return href === (window.location.origin + path);
+}
+
 function loop() {
     document.querySelectorAll('div[role=dialog]').forEach(e => {
-        if (e.innerText.includes('Lass dir nichts Neues entgehen')) {
+        const hasLoginButton = Array.from(e.querySelectorAll('a')).some(a => equalsLink(a.href, '/login'));
+        const hasSignUpButton = Array.from(e.querySelectorAll('a')).some(a => equalsLink(a.href, '/i/flow/signup'));
+
+        if (hasLoginButton && hasSignUpButton) {
+            console.log('twitter remove login banner')
             e.remove();
             document.querySelector('html').style.overflow = 'initial';
         }
