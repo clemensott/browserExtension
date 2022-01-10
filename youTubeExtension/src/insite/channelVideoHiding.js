d8a843c20ff646a05ecd9e15b86ecb1c80a36d17
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2022-01-10 06:14:02 +0100

    improved finished fetching detection

--- youTubeExtension/src/insite/channelVideoHiding.js
+++ youTubeExtension/src/insite/channelVideoHiding.js
@@ -1,6 +1,4 @@
 import KeysTracker from '../utils/KeysTracker';
-import tryIgnore from '../utils/tryIgnore';
-import fetchIntersectorService from '../Services/FetchIntersectorService';
 import './channelVideoHiding.css';
 
 const constants = {
@@ -12,7 +10,6 @@ const constants = {
 
 let intervalId = null;
 let isWatingToCoolDown = false;
-let finishedFetchingVideosUrl = null;
 const updatingVideoIds = new KeysTracker();
 
 function isOnChannelVideosSite() {
@@ -28,22 +25,23 @@ function getContainer() {
 }
 
 function getCurrentFetchedVideosCount(container) {
-    return container.querySelectorAll('ytd-grid-video-renderer').length;
+    return container.childElementCount;
 }
 
-function checkHideCount() {
+function finisedFetchingVideos(container) {
+    return !container.querySelector('ytd-continuation-item-renderer');
+}
+
+function checkHideCount(log = false) {
     const container = getContainer();
     if (!container) return;
 
-    if (finishedFetchingVideosUrl && finishedFetchingVideosUrl !== window.location.href) {
-        finishedFetchingVideosUrl = null;
-        console.log('reset finishedFetchingVideosUrl');
-    }
-
-    if (intervalId && !finishedFetchingVideosUrl && isOnChannelVideosSite()) {
+    if (intervalId && !finisedFetchingVideos(container) && isOnChannelVideosSite()) {
         const updateCount = updatingVideoIds.totalCount();
-        const videosCount = getCurrentFetchedVideosCount(container);
-        console.log('hiding:', isWatingToCoolDown, updateCount, videosCount);
+        if (log) {
+            const videosCount = getCurrentFetchedVideosCount(container);
+            console.log('hiding:', { isWatingToCoolDown, updateCount, videosCount });
+        }
 
         if (updateCount >= constants.UPDATE_COUNT_UPPER_THRESHOLD ||
             (isWatingToCoolDown && updateCount > constants.UPDATE_COUNT_LOWER_THRESHOLD)) {
@@ -51,14 +49,11 @@ function checkHideCount() {
             container.classList.remove(constants.HIDE_ALL_VIDEOS_CLASSNAME);
 
             isWatingToCoolDown = true;
-        } else if (videosCount >= 60) { // when at least the inital 60 videos are loaded
+        } else {
             container.classList.remove(constants.HIDE_NOT_ALL_VIDEOS_CLASSNAME);
             container.classList.add(constants.HIDE_ALL_VIDEOS_CLASSNAME);
             window.scrollTo(0, Math.random() * 400); // scroll to trigger loading next videos
             isWatingToCoolDown = false;
-        } else {
-            container.classList.remove(constants.HIDE_NOT_ALL_VIDEOS_CLASSNAME);
-            container.classList.remove(constants.HIDE_ALL_VIDEOS_CLASSNAME);
         }
     } else {
         container.classList.remove(constants.HIDE_NOT_ALL_VIDEOS_CLASSNAME);
@@ -66,30 +61,10 @@ function checkHideCount() {
     }
 }
 
-function onFetchText({ detail: { url, text } }) {
-    if (url.startsWith('https://www.youtube.com/youtubei/v1/browse')) {
-        try {
-            const data = JSON.parse(text);
-            const channelVideos = tryIgnore(() => data?.onResponseReceivedActions?.
-                map(a => a?.appendContinuationItemsAction?.continuationItems).find(Boolean));
-
-            const lastVideo = channelVideos[channelVideos.length - 1];
-            if (lastVideo && lastVideo.gridVideoRenderer) {
-                finishedFetchingVideosUrl = window.location.href;
-                console.log('finished fetching for:', finishedFetchingVideosUrl);
-                checkHideCount();
-            }
-        } catch (e) {
-        }
-    }
-}
-
 document.addEventListener('updateSources.startHandleVideos', ({ detail: videos }) => {
     updatingVideoIds.add(videos.map(v => v.id));
 
-    if (intervalId) {
-        checkHideCount();
-    }
+    setTimeout(() => intervalId && checkHideCount(), 100);
 });
 
 document.addEventListener('updateSources.endUpdateThumbnails', ({ detail: videoIds }) => {
@@ -102,23 +77,14 @@ document.addEventListener('updateSources.endUpdateThumbnails', ({ detail: videoI
 
 function startHiding() {
     stopHiding();
+    intervalId = setInterval(() => checkHideCount(true), 5000);
 
-    intervalId = setInterval(() => checkHideCount(), 5000);
-    fetchIntersectorService.addOnTextListener(onFetchText);
-
-    const videoCount = isOnChannelVideosSite() && getContainer() && getCurrentFetchedVideosCount(getContainer());
-    if (videoCount % 30 !== 0) {
-        // is on channel video site and all videos are laoded because videos are loaded in batches of 30 videos
-        finishedFetchingVideosUrl = window.location.href;
-    }
-
-    checkHideCount();
+    checkHideCount(true);
 }
 
 function stopHiding() {
     intervalId && clearInterval(intervalId);
     intervalId = null;
-    fetchIntersectorService.removeOnTextListener(onFetchText);
 
     checkHideCount();
 }
