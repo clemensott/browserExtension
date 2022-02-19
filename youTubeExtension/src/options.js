38336e3473329985a553f204637a80879902fad8
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2022-02-20 00:57:12 +0100

    removed log statement

--- youTubeExtension/src/options.js
+++ youTubeExtension/src/options.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/options/App';
 
-console.log('Hello Options');
-
 ReactDOM.render(<App />, document.getElementById('root'));
