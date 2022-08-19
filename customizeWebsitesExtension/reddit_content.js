4cf578ccc4e6c1f30659a8e14e9773969435eba8
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2022-08-20 00:09:09 +0200

    remove reddit console logs

--- customizeWebsitesExtension/reddit_content.js
+++ customizeWebsitesExtension/reddit_content.js
@@ -1,5 +1,4 @@
 function muteVideo({ target }) {
-    console.log('mute:', target.volume);
     target.volume = 0;
 }
 
@@ -8,7 +7,6 @@ setInterval(() => {
         'div > div > div[id^=t3_z] div[data-isvideoplayer] > video:not([rdt-extension-muted])'
     );
     adPlayers.forEach(video => {
-        console.log('found:', video.volume);
         video.addEventListener('volumechange', muteVideo);
         video.volume = 0;
         video.setAttribute('rdt-extension-muted', 1);
