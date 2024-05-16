0b540643672c992982a091219a77abbb45f70299
Clemens Ott <42971547+clemensott@users.noreply.github.com>
2024-05-16 06:53:42 +0200

    [CS] fix issues with in instagram content script

--- customizeWebsitesExtension/instagram_content.js
+++ customizeWebsitesExtension/instagram_content.js
@@ -1,95 +1,157 @@
-const intervalIds = {};
-let lastUrl = null;
-
-
+const intervalIds = new Map();
+const lastState = {
+    url: null,
+    element: null,
+    text: null,
+};
 
 function clearIntervalByName(name) {
-    console.log('clear interval by name:', name, intervalIds[name]);
-    if (intervalIds[name]) {
-        clearInterval(intervalIds[name]);
-        delete intervalIds[name];
+    console.log('clear interval by name:', name, intervalIds.get(name));
+    if (intervalIds.has(name)) {
+        clearInterval(intervalIds.get(name));
+        delete intervalIds.delete(name);
     }
-}
-
-function clearAllIntervals() {
-    Object.keys(intervalIds).forEach(clearIntervalByName);
 }
 
 function setupInterval(intervalName, callback, timeout) {
-    intervalIds[intervalName] = setInterval(callback, timeout, intervalName);
+    clearIntervalByName(intervalName);
+    intervalIds.set(intervalName, setInterval(callback, timeout, intervalName));
+}
+
+function setHidden(element) {
+    element.dataset.extensionHidden = '';
 }
 
 function setupIntervals() {
-    function getLoginOverlay() {
-        for (let i = document.body.childElementCount - 1; i >= 0; i--) {
-            const child = document.body.children.item(i);
-            if (child.querySelector('input[name="username"]')) {
-                return child;
-            }
-        }
-        return null;
+    function isLoginBottemBannerCloseButton(element) {
+        return !!element.parentElement.querySelector('a[role=link][href*="/accounts/login"]');
     }
 
-    if (!window.location.href.startsWith('https://www.instagram.com/accounts/login')) {
-        let removedLoginOverlay = false;
-        setupInterval('loginInterval', intervalName => {
-            const overlayBaseElement = getLoginOverlay();
-            if (overlayBaseElement) {
-                overlayBaseElement.style.setProperty('display', 'none');
-                // removedLoginOverlay = true;
-            }
-            if (removedLoginOverlay && document.body.style.overflow) {
-                document.body.style.overflow = null;
-                clearIntervalByName(intervalName);
-            }
-        }, 100);
+    function getLoginBottomBannerCloseButton() {
+        return [...document.querySelectorAll('button > span[aria-label="Close"]')]
+            .map(element => element.parentElement)
+            .find(isLoginBottemBannerCloseButton);
     }
 
-    // The div element that lies over the main image to avoid downloading
-    function getMainImageOverlay() {
-        const overlay = document.querySelector("div > div[role=button] > div > div > div:nth-child(2)");
-        return overlay &&
-            overlay.parentNode.children[0] &&
-            overlay.parentNode.children[0].children[0] &&
-            overlay.parentNode.children[0].children[0].tagName === 'IMG' ? overlay : null;
-    }
-
-    setupInterval('mainImageOverlay', intervalName => {
-        const overlayElement = getMainImageOverlay();
-        if (overlayElement) {
-            overlayElement.remove();
+    setupInterval('loginBottomBanner', intervalName => {
+        const loginBottomBanner = getLoginBottomBannerCloseButton();
+        if (loginBottomBanner) {
+            loginBottomBanner.click();
             clearIntervalByName(intervalName);
         }
     }, 100);
 
-    function getCookieBannerOverlay() {
-        for (let i = document.body.childElementCount - 1; i >= 0; i--) {
-            const child = document.body.children.item(i);
-            if (child.tagName !== 'DIV') {
-                continue;
-            }
-            const cookieCount = child.innerText.toLowerCase().split('cookie').length;
-            if (cookieCount > 10) {
-                return child;
-            }
-        }
-        return null;
+    // The div element that lies over the main image to avoid downloading
+    function getMainImageOverlays() {
+        return [...document.querySelectorAll('img[alt^="Photo shared by"]')]
+            .map(img => img.parentElement.nextSibling);
     }
 
-    setupInterval('removeCookieBanner', intervalName => {
-        const overlayElement = getCookieBannerOverlay();
-        if (overlayElement) {
-            overlayElement.style.setProperty('display', 'none');
+    setupInterval('mainImageOverlay', intervalName => {
+        const overlayElements = getMainImageOverlays();
+        if (overlayElements.length) {
+            overlayElements.forEach(setHidden);
             clearIntervalByName(intervalName);
         }
     }, 100);
 }
 
+function isLoginOverlay(element) {
+    return element.querySelector('input[name="username"]');
+}
+
+function isCookieBanner(element) {
+    const cookieCount = element.innerText.toLowerCase().split('cookie', 10).length;
+    return cookieCount >= 10;
+}
+
+function setupBodyElementObserver() {
+    const observer = new MutationObserver(changes => changes
+        .flatMap(({ addedNodes }) => [...(addedNodes || [])])
+        .filter(element => isLoginOverlay(element) || isCookieBanner(element))
+        .forEach(setHidden));
+
+    observer.observe(document.body, {
+        childList: true,
+    });
+}
+
+function sanitizeString(str) {
+    return typeof str === 'string' ? str
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/>/g, '&gt;')
+        .replace(/</g, '&lt;')
+        : str;
+}
+
+function runFunctionInsite(func) {
+    const code = `(${func.toString()})();`;
+    const isFirefox = typeof InstallTrigger !== 'undefined';
+    if (isFirefox) {
+        window.eval(code);
+    } else {
+        const div = document.createElement('div');
+        div.innerHTML = `<svg onload="eval(this.dataset.code)" data-code="${sanitizeString(code)}" />`;
+        div.style.display = 'none';
+        document.body.appendChild(div);
+    }
+}
+
+function setupVideoPlaybackInterval() {
+    runFunctionInsite(function () {
+        setInterval(() => {
+            const video = document.querySelector('video');
+            if (video && !video.oldPause) {
+                let lastOverlayClicked = 0;
+
+                video.oldPause = video.pause;
+                video.pause = () => {
+                    if (Date.now() - lastOverlayClicked < 50) video.oldPause();
+                };
+
+                const overlay = video.nextSibling.querySelector('div[role="presentation"]');
+                const thumbnail = video.nextSibling.querySelector('img');
+                overlay.addEventListener('click', () => {
+                    if (thumbnail) {
+                        thumbnail.style.display = 'none';
+                    }
+                    if (video.paused) video.play();
+                    else {
+                        video.oldPause();
+                        lastOverlayClicked = Date.now();
+                    }
+                });
+            }
+        }, 1500);
+    });
+}
+
+
+function changedPage() {
+    if (window.location.href === lastState.url) {
+        return false;
+    }
+
+    const element = document.contains(lastState.element)
+        ? lastState.element : document.querySelector('h1');
+    if (element.innerText === lastState.text) {
+        return false;
+    }
+
+    lastState.url = window.location.href;
+    lastState.element = element;
+    lastState.innerText = element.innerText;
+
+    return true;
+}
+
 setInterval(() => {
-    if (window.location.href !== lastUrl) {
+    if (changedPage()) {
         console.log('reload intervals');
-        lastUrl = window.location.href;
-        clearAllIntervals();
-        setupIntervals();
+        setTimeout(setupIntervals, 100);
     }
 }, 100);
+
+setupBodyElementObserver();
+setupVideoPlaybackInterval();
