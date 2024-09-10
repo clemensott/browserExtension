a1d93a8631d7bded75df2bd0cae761974502ddf2
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2024-09-10 21:13:28 +0200

    [YT] fix video id dection of video container

--- youTubeExtension/src/utils/getVideoIdFromVideoContainer.js
+++ youTubeExtension/src/utils/getVideoIdFromVideoContainer.js
@@ -1,6 +1,6 @@
 import getVideoIdFromUrl from './getVideoIdFromUrl';
 
 export default function getVideoIdFromVideoContainer(container) {
-    const a = container.querySelector('a#thumbnail');
+    const a = container.querySelector('a#thumbnail,a.reel-item-endpoint');
     return a ? getVideoIdFromUrl(a.href) : null;
 }
