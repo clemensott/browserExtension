b552b7d14d909c888539dda3a1613d290a1a1867
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2025-07-08 21:11:25 +0200

    [YT] implement yt-lockup-view-model video container

--- youTubeExtension/src/utils/getVideoIdFromVideoContainer.js
+++ youTubeExtension/src/utils/getVideoIdFromVideoContainer.js
@@ -1,6 +1,6 @@
 import getVideoIdFromUrl from './getVideoIdFromUrl';
 
 export default function getVideoIdFromVideoContainer(container) {
-    const a = container.querySelector('a#thumbnail,a.reel-item-endpoint');
+    const a = container.querySelector('a#thumbnail,a[href^="/watch?"],a.reel-item-endpoint');
     return a ? getVideoIdFromUrl(a.href) : null;
 }
